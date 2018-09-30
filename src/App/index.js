d66ac7d354799024d3bf7330cfbb74dab4762fe7
Will Mitchell <wvmitchell@gmail.com>
2018-09-30 13:38:34 -0600

    update snapshot

--- src/App/index.js
+++ src/App/index.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <AddVeggiesForm />
+        <AddVeggiesForm addVegetable={this.addVegetable} />
       </div>
     );
   }
