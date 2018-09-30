7f3f4b032af972797df5b521c699dec179d1aab8
Will Mitchell <wvmitchell@gmail.com>
2018-09-30 13:32:05 -0600

    remove boilerplate, and update snapshot

--- src/App/index.js
+++ src/App/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+import AddVeggiesForm from '../AddVeggiesForm'
 import './App.css';
 
 class App extends Component {
@@ -13,13 +13,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <AddVeggiesForm />
       </div>
     );
   }
